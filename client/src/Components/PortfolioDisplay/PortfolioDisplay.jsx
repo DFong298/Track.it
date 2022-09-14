04ce6de97ddaf60802a160bc1770a24f59027361
DFong298 <dennis1.fong@gmail.com>
2022-09-14 15:30:34 -0400

    Finished structure for table to display table

--- client/src/Components/PortfolioDisplay/PortfolioDisplay.jsx
+++ client/src/Components/PortfolioDisplay/PortfolioDisplay.jsx
@@ -33,37 +33,54 @@ const PortfolioDisplay = () => {
     return(
         <div className='portfolios-page'>
             <Sidebar/>
-            <div className='table_headers'></div>
-            <Link to='/addstock' style={{textDecoration: 'none'}}>
-                <button className='add-stock-button'> + Add Stock</button>
-            </Link>
-            <TableContainer classname="data-table" component={Paper} style={{backgroundColor: '#FFFDD0', width: '65%', position: 'relative', left: '19%', borderColor: '#8F3F3F'}} sx={{border: 3, borderRadius: '16px'}}>
+            <TableContainer classname="data-table" component={Paper} style={{backgroundColor: 'white', 
+                                                                            width: '65%', 
+                                                                            maxHeight: '70%', 
+                                                                            position: 'relative', 
+                                                                            left: '18.5%', 
+                                                                            top: '12%',
+                                                                            borderColor: '#8F3F3F'}} 
+                                                                            sx={{border: 3, borderRadius: '16px'}}>
                 <Table style={{ width: 1200, margin: 'auto' }} sx={{ minWidth: '400px' }}>
                     <TableHead>
                     <TableRow>
                         <TableCell align="center" style={{color: '#8F3F3F', fontSize: "20px", fontWeight: "700"}}>Ticker</TableCell>
                         <TableCell align="center" style={{color: '#8F3F3F', fontSize: "20px", fontWeight: "700"}}>Company Name</TableCell>
                         <TableCell align="center" style={{color: '#8F3F3F', fontSize: "20px", fontWeight: "700"}}>Number of Shares</TableCell>
-                        <TableCell align="center" style={{color: '#8F3F3F', fontSize: "20px", fontWeight: "700"}}>Price Bought</TableCell>
-                        <TableCell align="center" style={{color: '#8F3F3F', fontSize: "20px", fontWeight: "700"}}>Current Price</TableCell>
+                        <TableCell align="center" style={{color: '#8F3F3F', fontSize: "20px", fontWeight: "700"}}>Price Bought &#40;$&#41;</TableCell>
+                        <TableCell align="center" style={{color: '#8F3F3F', fontSize: "20px", fontWeight: "700"}}>Current Price &#40;$&#41;</TableCell>
                         <TableCell align="center" style={{color: '#8F3F3F', fontSize: "20px", fontWeight: "700"}}>Day's Change &#40;%&#41;</TableCell>
                         <TableCell align="center" style={{color: '#8F3F3F', fontSize: "20px", fontWeight: "700"}}>Total Change &#40;%&#41;</TableCell>
-                        <TableCell align="center" style={{color: '#8F3F3F', fontSize: "20px", fontWeight: "700"}}>Total Value</TableCell>
+                        <TableCell align="center" style={{color: '#8F3F3F', fontSize: "20px", fontWeight: "700"}}>Total Value &#40;$&#41;</TableCell>
                     </TableRow>
                     </TableHead>
                     <TableBody>
                     {data.map((item) => (
                         <TableRow>
-                            <TableCell align='center' width='12%' >{item.ticker}</TableCell>
+                            <TableCell align='center' width='12%'>{item.ticker}</TableCell>
                             <TableCell align="center" width='12%'>{item.stockname}</TableCell>
                             <TableCell align="center" width='12%'>{item.numshares}</TableCell>
-                            <TableCell align="center" width='12%'>{item.pricebought}</TableCell>
-                            <TableCell align="center" width='12%'>{item.currentprice}</TableCell>
+                            <TableCell align="center" width='12%'>${item.pricebought}</TableCell>
+                            <TableCell align="center" width='12%'>${item.currentprice}</TableCell>
                             <TableCell align="center" width='12%'>{item.daychange} %</TableCell>
                             <TableCell align="center" width='12%'>{item.totalchange} %</TableCell>
-                            <TableCell align="center" width='12%'>{item.totalvalue}</TableCell>
+                            <TableCell align="center" width='12%'>${item.totalvalue}</TableCell>
                         </TableRow>
                     ))}
+                    <TableRow>
+                        <TableCell align="left" width='12%'>
+                            <Link to='/addstock' style={{textDecoration: 'none'}}>
+                                <button className='add-stock-button'> + Add Stock</button>
+                            </Link>
+                        </TableCell>
+                        <TableCell align="center" width='12%'></TableCell>
+                        <TableCell align="center" width='12%'></TableCell>
+                        <TableCell align="center" width='12%'></TableCell>
+                        <TableCell align="center" width='12%'></TableCell>
+                        <TableCell align="center" width='12%'>Total Day %</TableCell>
+                        <TableCell align="center" width='12%'>Total Portfolio %</TableCell>
+                        <TableCell align='center' width='12%'>Portfolio Value</TableCell>
+                    </TableRow>
                     </TableBody>
                 </Table>
             </TableContainer>
